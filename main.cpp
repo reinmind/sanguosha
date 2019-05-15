babbfa45829fb0789e595c99a0bb3257f73f20a5
blukat <acjason@163.com>
2019-05-15 16:35:25 +0800

    init

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include "player/player.h"
+int main(){
+	player p1;
+	p1.judge_stage();
+	p1.draw_stage();
+	p1.play_stage();
+	p1.discard_stage();
+}
---
+++ player/player.cpp
@@ -0,0 +1,18 @@
+#include "player.h"
+#include <iostream>
+int player::judge_stage(){
+	std::cout << "judge stage" << std::endl; 
+	return 1;
+}
+int player::draw_stage(){
+	std::cout << "draw stage" << std::endl;
+	return 1;
+}
+int player::play_stage(){
+	std::cout << "play stage" << std::endl;
+	return 1;
+}
+int player::discard_stage(){
+	std::cout << "discard stage" << std::endl;
+	return 1;
+}
